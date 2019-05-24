5c0a83d62ea972636c71ad88534c2deb3f6ef41c
Magda Adamczyk <magdii7@op.pl>
2019-05-24 21:54:55 +0200

    gui start for lab3

--- SMPD.py
+++ SMPD.py
@@ -183,7 +183,7 @@ main.minsize(800, 500)
 selected_method = tkinter.StringVar("")
 # Defines and places the notebook widget
 notebook = ttk.Notebook(main)
-notebook.grid(row=1, column=0, columnspan=50, rowspan=49, sticky='NESW')
+notebook.grid(row=1, column=0, columnspan=40, rowspan=49, sticky='NESW')
 
 # Adds tab 1 of the notebook
 page1 = ttk.Frame(notebook)
@@ -198,7 +198,7 @@ load_button.grid(row=0, column=0, padx=20, pady=20, sticky="nw")
 
 # choose how many features
 combobox = ttk.Combobox(page1, state="readonly",
-                        values=[i for i in range(1,65)])
+                        values=[i for i in range(1, 65)])
 combobox.grid(row=0, column=1, sticky="n", padx=20, pady=20)
 
 # choose if fisher or sfs
@@ -220,6 +220,48 @@ calculate_button.grid(row=0, column=2, padx=20, pady=20, sticky="nw")
 
 # Adds tab 2 of the notebook
 page2 = ttk.Frame(notebook)
+selected_classify_method = tkinter.StringVar("")
+
 notebook.add(page2, text='Classifiers')
+
+load_button = ttk.Button(page2, text="Load file", cursor="hand2", command=lambda: loadFile())
+load_button.grid(row=0, column=0, padx=20, pady=20, sticky="nw")
+
+# choose classifier method
+classifier_method_label = ttk.Label(page2, text="Choose method: ", justify="left")
+classifier_method_label.grid(row=1, column=0, sticky="nw", padx=20, pady=20)
+NN_radiobutton = ttk.Radiobutton(page2, text="NN",  value="NN",
+                                                variable=selected_classify_method, cursor="hand2")
+NN_radiobutton.grid(row=2, column=0, sticky="nw", padx=20)
+kNN_radiobutton = ttk.Radiobutton(page2, text="kNN",  value="kNN", variable=selected_classify_method,
+                                                cursor="hand2")
+kNN_radiobutton.grid(row=3, column=0, sticky="nw", padx=20)
+
+NM_radiobutton = ttk.Radiobutton(page2, text="NM",  value="NM", variable=selected_classify_method,
+                                                cursor="hand2")
+NM_radiobutton.grid(row=4, column=0, sticky="nw", padx=20)
+
+kNM_radiobutton = ttk.Radiobutton(page2, text="kNM",  value="kNM", variable=selected_classify_method,
+                                                cursor="hand2")
+kNM_radiobutton.grid(row=5, column=0, sticky="nw", padx=20)
+
+# train controls
+train_button = ttk.Button(page2, text="Train", cursor="hand2")
+train_button.grid(row=6, column=0, padx=20, pady=20, sticky="nw")
+train_label = ttk.Label(page2, text="Training part (%):", justify="left")
+train_label.grid(row=6, column=1, sticky="nw", padx=20, pady=20)
+train_entry = ttk.Entry(page2, width=20)
+train_entry.grid(row=6, column=2, sticky="nw", padx=20, pady=20)
+
+# execute button
+execute_button = ttk.Button(page2, text="Execute", cursor="hand2")
+execute_button.grid(row=7, column=0, padx=20, pady=20, sticky="nw")
+
+listbox_classify = tkinter.Listbox(page2, activestyle="none",
+                    height=30, width=70)
+listbox_classify.grid(row=0, column=3, rowspan=50, padx=20, pady=20)
+
+
+
 
 main.mainloop()
